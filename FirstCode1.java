d1a5ccb3a64cb859a5bed8ccfab8758f306a5769
KhaidirKhaizuki <72853034+KhaidirKhaizuki@users.noreply.github.com>
2022-06-30 13:53:26 +0800

    Add files via upload

---
+++ FirstCode1.java
@@ -0,0 +1,26 @@
+
+public class FirstCode {
+	  public static void main(String[] args) {
+		    System.out.println("Hello World!");
+		    System.out.println("I am learning Java.");
+		    System.out.println("It is awesome!");
+		    System.out.println(3 + 3);
+		    
+		    String name = "John";
+		    System.out.println(name);
+		    
+		    int myNum = 15;
+		    myNum = 20;  // myNum is now 20
+		    System.out.println(myNum);
+		    
+		    
+		    float myFloatNum = 5.99f;
+		    char myLetter = 'D';
+		    boolean myBool = true;
+		    String myText = "Hello";
+		    
+		    int x = 5;
+		    int y = 6;
+		    System.out.println(x + y); // Print the value of x + y
+		  }
+}
