22f90d1d89b110363a1cdea83510ba640b96b7f0
KhaidirKhaizuki <72853034+KhaidirKhaizuki@users.noreply.github.com>
2022-06-30 11:14:54 +0800

    Add files via upload

---
+++ HackerRank Java Stdin and Stdout II.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class solution3 {
+	public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter number please:");
+        int i = scan.nextInt();
+        System.out.println("Enter float number please:");
+        double d = scan.nextDouble();
+        System.out.println("Enter string please:");
+        String s = scan.nextLine();
+        s = scan.nextLine();
+        // Write your code here.
+        System.out.println("Int: " + i);
+        System.out.println("Double: " + d);
+        System.out.println("String: " + s);       
+    }
+}
