3d8c13f573109455cf9040fd99c3af06e1e3b0f9
KhaidirKhaizuki <72853034+KhaidirKhaizuki@users.noreply.github.com>
2022-06-30 11:02:44 +0800

    Add files via upload

---
+++ Java Stdin and Stdout I – Hacker Rank Solution.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Solution {
+	public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter 3 numbers");
+        
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
